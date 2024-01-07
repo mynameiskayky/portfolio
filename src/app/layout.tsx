8ad5b2a24abba6f9111c3b769dec06651f6e1a99
nynameiskayky <kaykyvieiraa@hotmail.com>
2024-01-07 13:05:23 -0300

    chore: initial project configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import ClashDisplay from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = ClashDisplay({ src: "../fonts/ClashDisplay-Regular.woff2" });
 
 export const metadata: Metadata = {
   title: "portfolio",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
