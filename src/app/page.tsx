1d676365886dd2b6a5ef0cd95a03e5170ba4bdcb
nynameiskayky <kaykyvieiraa@hotmail.com>
2024-01-17 02:26:11 -0300

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,56 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full font-bold justify-center flex-col border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          here begins my space on the internet.
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://www.linkedin.com/in/kaykyvieraa/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <code className="font-mono italic font-thin">Code by @Kayky</code>
-          </a>
+    <main className="">
+      <div className="border-b border-neutral-700">
+        <div className="mx-auto grid h-12 max-w-7xl grid-cols-12">
+          <div className="col-span-4 border-x border-neutral-700" />
+          <div className="col-span-3 border-r border-neutral-700" />
+          <div className="col-span-1 border-r border-neutral-700" />
+          <div className="col-span-4 border-r border-neutral-700" />
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className="text-4xl">I&#39;m a Software Engineer</h1>
-      </div>
+      <header className="mx-auto relative grid max-w-7xl grid-cols-12 border-l border-neutral-700">
+        <div className="relative flex w-full">
+          <h1 className="text-[10.25rem] w-fit z-50 text-neutral-100 font-bold pl-8 -tracking-wider">
+            Kayky.
+          </h1>
+          <span className="px-4 py-2 rounded-full whitespace-nowrap h-fit mt-8 font-bold text-base bg-[#E1FF2D] text-[#294B07]">
+            Good Night!
+          </span>
+        </div>
 
-      <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://www.instagram.com/mynameiskayky"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Instagram{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
+        <div className="col-span-3 border-r border-neutral-700" />
+        <div className="col-span-3 border-r border-neutral-700" />
+        <div className="col-span-1 border-r border-neutral-700" />
 
-        <a
-          href="https://www.linkedin.com/in/kaykyvieraa/"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            LinkedIn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
+        <div className="col-span-4 flex flex-col p-8 gap-4 justify-end items-end border-r border-neutral-700">
+          <div className="flex items-center gap-2">
+            <div className="h-2 w-2 animate-pulse rounded-full bg-[#74DA0D]" />
+            <p className="text-neutral-500 font-medium text-sm">
+              Codando && Ouvindo
+            </p>
+          </div>
+          <div className="h-[1px] w-11/12 bg-neutral-700" />
+          <div className="flex items-center gap-5">
+            <div className="text-right">
+              <h4 className="font-bold text-lg text-neutral-100">
+                Você Está Com Febre?
+              </h4>
+              <p className="font-medium text-sm text-neutral-400">
+                Febre90s, PumaPjl
+              </p>
+            </div>
+            <img
+              className="h-12 w-12 rounded-full animate-[spin_3s_linear_infinite]"
+              src="https://i.scdn.co/image/ab67616d0000b2730bb4807628d14055f19141af"
+              alt="Você Está Com Febre?"
+            />
+          </div>
+        </div>
+      </header>
 
-        <a
-          href="https://leetcode.com/mynameiskayky"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            LeetCode{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
-
-        <a
-          href="https://twitter.com/kay_vieiraa"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            X{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </a>
-      </div>
+      <div className="border-b w-screen border-neutral-700" />
     </main>
   );
 }
